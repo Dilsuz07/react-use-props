58dfbd8a993b9847421adc0e010d93e4f53b46eb
dilsuz <name@email>
2023-06-07 19:13:12 +0500

    Propslar bilan ishladim

---
+++ src/Components/Person/Person.js
@@ -0,0 +1,10 @@
+
+const Person = ({name,lastName}) => {
+    // console.log(props);
+    return <div>
+        <h1>Hello , My name is {name},Lastname {lastName}</h1>
+        <p>Hello world</p>
+    </div>
+}
+
+export default Person
