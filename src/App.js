937ec925301d18f281ffedf2891cf6ca7e329e7a
dilsuz <name@email>
2023-06-07 19:21:40 +0500

    link qushildi

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ function App() {
 
   return (
     <div className="App">
-      <Person name='Samir' lastName='Saidov' />
-      <Person name='Nigina' lastName='Zokirova' />
-      <Person name='Anvar' lastName='Nasullayev'/>
-      <Person name='Ilhom' lastName='Islomov' />
+      <Person name='Samir' lastName='Saidov' link={"#"} />
+      <Person name='Nigina' lastName='Zokirova' link={"#"} />
+      <Person name='Anvar' lastName='Nasullayev' link={"#"}/>
+      <Person name='Ilhom' lastName='Islomov' link={"#"}/>
     </div>
   );
 }
--- src/Components/Person/Person.js
+++ src/Components/Person/Person.js
@@ -1,8 +1,8 @@
 
-const Person = ({name,lastName}) => {
+const Person = ({name,lastName,link}) => {
     // console.log(props);
     return <div>
-        <h1>Hello , My name is {name},Lastname {lastName}</h1>
+        <h1>Hello , My name is {name},Lastname {lastName} ,{link}</h1>
         <p>Hello world</p>
     </div>
 }
